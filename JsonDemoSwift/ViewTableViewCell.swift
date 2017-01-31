bfaaabb04f253aa10fb8ac3a864e2fa19892c8af
ronak <ronak@Ronakjs-iMac.local>
2017-01-31 18:35:16 +0530

    first commit

---
+++ JsonDemoSwift/ViewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ViewTableViewCell.swift
+//  JsonDemoSwift
+//
+//  Created by heli on 1/27/17.
+//  Copyright © 2017 com.zaptechsolution. All rights reserved.
+//
+
+import UIKit
+
+class ViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
